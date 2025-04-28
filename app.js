2b21bcdcf46c95085b98754b3c30002dc781491c
bridgetorr17 <bridgetorr1902@gmail.com>
2025-04-28 16:33:44 -0400

    db layer

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const cors = require('mongodb');
 
 const app = express();
 const PORT = 8000;
+const MongoClient = mongodb.MongoClient;
 
 app.use(cors());
 
@@ -11,14 +13,29 @@ const obj = {
     "date": "April 28, 2025"
 }
 
-app.get('/', (req, res) => {
-    res.json(obj);
-})
+MongoClient.connect('mongodb+srv://bridgetorr1902:aGyiBmU0BQSZs1g6@cluster0.5payw8y.mongodb.net/Concerts?retryWrites=true&w=majority&appName=Cluster0')
+    .then(client => {
 
-app.get('/about', (req, res) => {
-    res.send('about route');
-})
+        const db = client.db('SurpriseSongs');
+        const concertCollection = db.collection('Concerts');
 
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+
+        app.get('/', (req, res) => {
+            concertCollection
+                .find()
+                .toArray()
+                .then(results => {
+                    results.sort((a,b) => b.votes - a.votes);
+                    response.json(results);
+                })
+                .catch(error => console.error(error));
+                })
+        
+        app.get('/about', (req, res) => {
+            res.send('about route');
+        })
+        
+        app.listen(PORT, () => {
+            console.log(`server is running on port ${PORT}`)
+        })
+    })

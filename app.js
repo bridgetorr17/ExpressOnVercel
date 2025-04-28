342f742421d1edcd1bba894e80f92d32779f014d
bridgetorr17 <bridgetorr1902@gmail.com>
2025-04-28 16:35:25 -0400

    syntax error in db code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const cors = require('mongodb');
+const mongodb = require('mongodb');
 
 const app = express();
 const PORT = 8000;
